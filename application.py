117d74ffc2c00a5f8131a3730f82a74654ee0756
JDSNX <judisonbacalso24@gmail.com>
2022-04-05 01:27:29 +0800

    index

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
 from flask import Flask
+import time
+
 application = Flask(__name__)
 
 @application.route('/')
-def hello_world():
-    return 'Sup madafaker!'
+def index():
+    return {"success": True, "timestamp": time.time()}
