a29566e446ed752481369f4a2cf6d0334788b6f3
JDSNX <judisonbacalso24@gmail.com>
2022-04-25 18:23:01 +0800

    added filter

--- application.py
+++ application.py
@@ -15,104 +15,124 @@ headers = {"Content-Type": "application.json"}
 def index():
     return render_template('index.html')
 
-@application.route('/get', methods=['GET'])
+@application.route('/get', methods=['GET', 'POST'])
 def get_all():
-    try:
-        resp = requests.get(url=f"{url}/users.json", headers=headers)
+    if request.method == 'POST':
+        return "Nuh-uh!"
 
-    except Exception as e:
-        return {"success": False, "msg": e, "timestamp": time.time()}
+    if request.method == 'GET':
+        try:
+            resp = requests.get(url=f"{url}/users.json", headers=headers)
 
-    return {"success": True, "result": resp.json(), "timestamp": time.time()}
+        except Exception as e:
+            return {"success": False, "msg": e, "timestamp": time.time()}
 
-@application.route('/get_user', methods=['GET'])
+        return {"success": True, "result": resp.json(), "timestamp": time.time()}
+
+@application.route('/get_user', methods=['GET', 'POST'])
 def get_user(ref_id=None):
-    try:
-        ref_id = str(request.args['ref_id'])
+    if request.method == 'POST':
+        return "Nuh-uh!"
+    
+    if request.method == 'GET':
+        try:
+            ref_id = str(request.args['ref_id'])
 
-        resp = requests.get(url=f"{url}/users/{ref_id}.json", headers=headers)
+            resp = requests.get(url=f"{url}/users/{ref_id}.json", headers=headers)
 
-        if resp.json() is None:
-            return {"success": False, "timestamp": time.time()}
+            if resp.json() is None:
+                return {"success": False, "timestamp": time.time()}
 
-    except Exception as e:
-        return {"success": False, "msg": e, "timestamp": time.time()}
+        except Exception as e:
+            return {"success": False, "msg": e, "timestamp": time.time()}
 
-    return {"success": True, "result": resp.json(), "timestamp": time.time()}
+        return {"success": True, "result": resp.json(), "timestamp": time.time()}
 
-@application.route('/update_pass', methods=['POST'])
+@application.route('/update_pass', methods=['GET', 'POST'])
 def update_pass():
-    try:
-        ref_id = str(request.args['ref_id'])
-        password = str(request.args['password'])
+    if request.method == 'GET':
+        return "Nuh-uh!"
 
-        user = get_user(ref_id)
-        if user["success"] is False:
-            return {"success": False, "msg": "User not found!", "timestamp": time.time()}
+    if request.method == 'POST':
+        try:
+            ref_id = str(request.args['ref_id'])
+            password = str(request.args['password'])
 
-        data = {"password": password, "timestamp": time.time()}
-        data = json.dumps(data, indent=4)
+            user = get_user(ref_id)
+            if user["success"] is False:
+                return {"success": False, "msg": "User not found!", "timestamp": time.time()}
 
-        resp = requests.patch(url=f"{url}/users/{ref_id}.json", headers=headers, data=data)
+            data = {"password": password, "timestamp": time.time()}
+            data = json.dumps(data, indent=4)
 
-        if resp.json() is None:
-            return {"success": False, "timestamp": time.time()}
+            resp = requests.patch(url=f"{url}/users/{ref_id}.json", headers=headers, data=data)
 
-    except Exception as e:
-        return {"success": False, "msg": e, "timestamp": time.time()}
+            if resp.json() is None:
+                return {"success": False, "timestamp": time.time()}
 
-    return {"success": True, "result": resp.reason, "timestamp": time.time()}
+        except Exception as e:
+            return {"success": False, "msg": e, "timestamp": time.time()}
 
-@application.route('/delete', methods=['GET'])
+        return {"success": True, "result": resp.reason, "timestamp": time.time()}
+
+@application.route('/delete', methods=['GET', 'POST'])
 def delete_user():
-    try:
-        ref_id = str(request.args['ref_id'])
+    if request.method == 'GET':
+        return "Nuh-uh!"
 
-        user = get_user(ref_id)
-        if user["success"] is False:
-            return {"success": False, "msg": "User not found!", "timestamp": time.time()}
+    if request.method == 'POST':
+        try:
+            ref_id = str(request.args['ref_id'])
 
-        resp = requests.delete(url=f"{url}/users/{ref_id}.json", headers=headers)
+            user = get_user(ref_id)
+            if user["success"] is False:
+                return {"success": False, "msg": "User not found!", "timestamp": time.time()}
 
-        if resp.json() is None:
-            return {"success": False, "timestamp": time.time()}
+            resp = requests.delete(url=f"{url}/users/{ref_id}.json", headers=headers)
 
-    except Exception as e:
-        return {"success": False, "msg": e, "timestamp": time.time()}
+            if resp.json() is None:
+                return {"success": False, "timestamp": time.time()}
 
-    return {"success": True, "result": resp.reason, "timestamp": time.time()}
+        except Exception as e:
+            return {"success": False, "msg": e, "timestamp": time.time()}
 
-@application.route('/update_user', methods=['POST'])
+        return {"success": True, "result": resp.reason, "timestamp": time.time()}
+
+@application.route('/update_user', methods=['GET', 'POST'])
 def update_user():
-    try:
-        ref_id = str(request.args['ref_id'])
-        first_name = str(request.args['first_name'])
-        last_name = str(request.args['last_name'])
-        middle_initial = str(request.args['middle_initial'])
-        emergency_contact_number = str(request.args['emergency_contact_number'])
-        emergency_contact_person = str(request.args['emergency_contact_person'])
+    if request.method == 'GET':
+        return "Nuh-uh!"
 
-        user = get_user(ref_id)
-        if user["success"] is False:
-            return {"success": False, "msg": "User not found!", "timestamp": time.time()}
+    if request.method == 'POST':
+        try:
+            ref_id = str(request.args['ref_id'])
+            first_name = str(request.args['first_name'])
+            last_name = str(request.args['last_name'])
+            middle_initial = str(request.args['middle_initial'])
+            emergency_contact_number = str(request.args['emergency_contact_number'])
+            emergency_contact_person = str(request.args['emergency_contact_person'])
 
-        data = {
-            "first_name": first_name, 
-            "last_name": last_name, 
-            "middle_initial": middle_initial, 
-            "emergency_contact_number": emergency_contact_number, 
-            "emergency_contact_person": emergency_contact_person, 
-            "timestamp": time.time()
-        }
-        
-        data = json.dumps(data, indent=4)
+            user = get_user(ref_id)
+            if user["success"] is False:
+                return {"success": False, "msg": "User not found!", "timestamp": time.time()}
 
-        resp = requests.patch(url=f"{url}/users/{ref_id}.json", headers=headers, data=data)
+            data = {
+                "first_name": first_name, 
+                "last_name": last_name, 
+                "middle_initial": middle_initial, 
+                "emergency_contact_number": emergency_contact_number, 
+                "emergency_contact_person": emergency_contact_person, 
+                "timestamp": time.time()
+            }
 
-        if resp.json() is None:
-            return {"success": False, "timestamp": time.time()}
+            data = json.dumps(data, indent=4)
 
-    except Exception as e:
-        return {"success": False, "msg": e, "timestamp": time.time()}
+            resp = requests.patch(url=f"{url}/users/{ref_id}.json", headers=headers, data=data)
 
-    return {"success": True, "result": resp.reason, "timestamp": time.time()}
+            if resp.json() is None:
+                return {"success": False, "timestamp": time.time()}
+
+        except Exception as e:
+            return {"success": False, "msg": e, "timestamp": time.time()}
+
+        return {"success": True, "result": resp.reason, "timestamp": time.time()}
